a16295ceeead26039e453063d62bc85552c0850f
WpMaster007 <wanderpimentels@gmail.com>
2025-04-27 21:34:36 +0000

    CARTA 1 E 2

--- Carta.c
+++ Carta.c
@@ -1,40 +1,80 @@
 #include <stdio.h>
 
 int main(){
-    printf ("Carta 1");
-    char Estado [1];
+    
+    char Estado [3];
     char Codigo [4];
-    char Cidade [40];
-    int Populacao, Pontos;
+    char Cidade [100];
     float Area;
     float PIB;
+    int Populacao, Pontos;
 
+    printf ("Carta 2 \n" );
     printf("Digite o Estado: ");
     scanf("%s", Estado);
-    printf("O Estado é: %s\n", Estado);
-
+    
     printf("Digite o Codigo: ");
     scanf("%s", Codigo);
-    printf("O Codigo é: %s\n", Codigo);
 
     printf("Digite a Cidade: ");
-    scanf("%s", Cidade);
-    printf("A Cidade é: %s\n", Cidade);
-
+    scanf(" %[^\n]", Cidade);
+    
     printf("Digite a População: ");
     scanf("%d", &Populacao);
-    printf("A População é: %d\n", Populacao);
+    
+    printf("Digite a Área: ");
+    scanf("%f", &Area);
+    
+    printf("Digite o PIB: ");
+    scanf("%f", &PIB);
 
     printf("Digite os Pontos Turísticos: ");
     scanf("%d", &Pontos);
+
+    printf("O Estado é: %s\n", Estado);
+    printf("O Codigo é: %s\n", Codigo);
+    printf("A Cidade é: %s\n", Cidade);
+    printf("A População é: %d\n", Populacao);
+    printf("A Área é: %.4f km²\n", Area);
+    printf("O PIB é: %.4f Bilhões de reais\n", PIB);
     printf("Os Pontos Turísticos são: %d\n", Pontos);
 
+    char Estado [3];
+    char Codigo [4];
+    char Cidade [100];
+    float Area;
+    float PIB;
+    int Populacao, Pontos;
+
+    printf ("Carta 2 \n" );
+    printf("Digite o Estado: ");
+    scanf("%s", Estado);
+    
+    printf("Digite o Codigo: ");
+    scanf("%s", Codigo);
+
+    printf("Digite a Cidade: ");
+    scanf(" %[^\n]", Cidade);
+    
+    printf("Digite a População: ");
+    scanf("%d", &Populacao);
+    
     printf("Digite a Área: ");
     scanf("%f", &Area);
-    printf("A Área é: %f\n", Pontos);
-
+    
     printf("Digite o PIB: ");
     scanf("%f", &PIB);
-    printf("O PIB é: %f\n", PIB);
+
+    printf("Digite os Pontos Turísticos: ");
+    scanf("%d", &Pontos);
+
+    printf("O Estado é: %s\n", Estado);
+    printf("O Codigo é: %s\n", Codigo);
+    printf("A Cidade é: %s\n", Cidade);
+    printf("A População é: %d\n", Populacao);
+    printf("A Área é: %.4f km²\n", Area);
+    printf("O PIB é: %.4f Bilhões de reais\n", PIB);
+    printf("Os Pontos Turísticos são: %d\n", Pontos);
+
     return 0;
 }
